810b032af083a78f96f60974343e64fa62be07aa
mrcgithub120689 <85733022+mrcgithub120689@users.noreply.github.com>
2025-04-10 14:06:50 +1000

    fix test workflow (#6)

--- Hub/tests/api/restful.api.dev.api.spec.ts
+++ Hub/tests/api/restful.api.dev.api.spec.ts
@@ -1,10 +1,12 @@
 import { expect } from '@playwright/test';
 import { test } from '../../hub-fixture';
+import { configEnv } from '../../../framework/configLoader';
 
 test.describe(`Sample API Tests`, {tag: ['@API', '@Sample']}, () => {
     test(`Get Objects`, {tag: ['@Get']}, async({restfulApiDevApi}) => {
         let objects = await restfulApiDevApi.getObjects();
-        // console.log(JSON.stringify(objects, null, 2));
+        console.log(process.env.ENV);
+        console.log(JSON.stringify(configEnv, null, 2));
         expect(objects.length).toEqual(13);
     });
 });
