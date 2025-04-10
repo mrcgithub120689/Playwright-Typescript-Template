a0663bd66a35b0af0d4a3bd08c2c92ff2727b414
mrcgithub120689 <85733022+mrcgithub120689@users.noreply.github.com>
2025-04-10 11:25:35 +1000

    separate workflows to compile and test (#3)

--- Hub/tests/api/restful.api.dev.api.spec.ts
+++ Hub/tests/api/restful.api.dev.api.spec.ts
@@ -2,9 +2,9 @@ import { expect } from '@playwright/test';
 import { test } from '../../hub-fixture';
 
 test.describe(`Sample API Tests`, {tag: ['@API', '@Sample']}, () => {
-    test(`Get Objects`, {tag: ['@Get'],}, async({restfulApiDevApi}) => {
+    test(`Get Objects`, {tag: ['@Get']}, async({restfulApiDevApi}) => {
         let objects = await restfulApiDevApi.getObjects();
-        console.log(JSON.stringify(objects, null, 2));
+        // console.log(JSON.stringify(objects, null, 2));
         expect(objects.length).toEqual(13);
     });
 });
